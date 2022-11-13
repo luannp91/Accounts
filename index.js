3ee6cc6dc40ac73eccf0ccc634e1bc284e8b7504
Luan Pinto <luan.nelmo@hotmail.com>
2022-11-12 21:27:24 -0300

    Inicializacao do sistema

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ const chalk = require('chalk')
 //  Modulos Internos
 const fs = require('fs')
 
-console.log('Iniciamos o Accounts')
+operation()
+
+function operation() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'O que você deseja fazer?',
+            choices: ['Criar Conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair'],
+        },
+    ]).then((answer) => {
+        const action = answer['action']
+        console.log(action)
+    }).catch((err) => console.log(err))
+}
