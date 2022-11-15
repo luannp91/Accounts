36a58ea520f19b421f95084462f0b26e71a02cb3
Luan Pinto <luan.nelmo@hotmail.com>
2022-11-14 21:06:03 -0300

    Adicao de Saldo

--- index.js
+++ index.js
@@ -89,7 +89,18 @@ function deposit() {
         if (!checkAccount(accountName)) {
             return deposit()
         }
-    })
+
+        inquirer.prompt([
+            {
+                name: 'amount',
+                message: 'Quanto você deseja depositar?',
+            },
+        ]).then((answer) => {
+            const amount = answer['amount']
+            addAmount(accountName, amount)
+            operation()
+        }).catch((err) => console.log(err))
+    }).catch((err) => console.log(err))
 }
 
 //  Verificar se a conta existe
@@ -99,4 +110,26 @@ function checkAccount (accountName) {
         return false
     }
     return true
+}
+
+function addAmount(accountName, amount) {
+    const accountData = getAccount(accountName)
+    if (!amount) {
+        console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'))
+        return deposit()
+    }
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
+    fs.writeFileSync(`accounts/${accountName}.json`, JSON.stringify(accountData), 
+    function (err) {
+        console.log(err)
+    },)
+    console.log(chalk.green(`Foi depositado o valor de R$${amount} na sua conta!`))
+}
+
+function getAccount(accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+        encoding: 'utf8',
+        flag: 'r'
+    })
+    return JSON.parse(accountJSON)
 }
